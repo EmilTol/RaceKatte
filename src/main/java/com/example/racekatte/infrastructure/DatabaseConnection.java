0ac8ba19ba7a6370b6a6c44c25e339008cd98277
Emil <erazertt@gmail.com>
2025-03-31 20:24:35 +0200

    TestTest

---
+++ src/main/java/com/example/racekatte/infrastructure/DatabaseConnection.java
@@ -0,0 +1,5 @@
+package com.example.racekatte.infrastructure;
+
+public class DatabaseConnection {
+
+}
---
+++ src/main/java/com/example/racekatte/presentation/LoginController.java
@@ -0,0 +1,4 @@
+package com.example.racekatte.presentation;
+
+public class LoginController {
+}
