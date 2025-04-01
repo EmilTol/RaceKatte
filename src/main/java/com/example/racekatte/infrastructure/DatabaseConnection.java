e6f59385f3aac2852a8098d6e7f00a5a572c3564
Emil <erazertt@gmail.com>
2025-04-01 09:01:22 +0200

    DatabaseConnection

--- src/main/java/com/example/racekatte/infrastructure/DatabaseConnection.java
+++ src/main/java/com/example/racekatte/infrastructure/DatabaseConnection.java
@@ -1,5 +1,20 @@
 package com.example.racekatte.infrastructure;
 
-public class DatabaseConnection {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import javax.sql.DataSource;
 
+@Configuration
+public class DatabaseConnection {
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://localhost:3306/pizzaParadise");
+        ds.setUsername("root");
+        ds.setPassword("root");
+        return ds;
+
+    }
 }
