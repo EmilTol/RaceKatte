53abb3e9ae0bde30e9220e1b03c6bdf7fdc39b81
Annika <annikarasmussen93@gmail.com>
2025-04-01 12:26:18 +0200

    nuværende html filer delt

--- src/main/java/com/example/racekatte/presentation/LoginController.java
+++ src/main/java/com/example/racekatte/presentation/LoginController.java
@@ -1,4 +1,25 @@
 package com.example.racekatte.presentation;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LoginController {
+
+    @GetMapping("/")
+    public String loginSide( Model model) {
+
+        return "login";
+    }
+
+    @GetMapping("/home")
+    public String homeTing( Model model) {
+        return "home";
+    }
+
+    @GetMapping("/user")
+    public String userTing( Model model) {
+        return "user";
+    }
 }
